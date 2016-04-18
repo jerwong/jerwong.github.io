a087b1b12dd767c83e8a47504d8a714c31397292
jw6644 <jeremy_wong67@hotmail.com>
2016-04-18 01:39:05 -0700

    Fixed

---
+++ js/caption.js
@@ -0,0 +1,27 @@
+$(window).on("load", function() {
+  $(".ic_container").capslide({
+      caption_color	: 'white',
+      caption_bgcolor	: '',
+      overlay_bgcolor : 'black',
+      border			: '',
+      showcaption	    : true,
+  });
+
+  $(window).on("resize", function() {
+    var container = $('.ic_container');
+    container.css({
+      width: "",
+      height: ""
+    });
+    window.setTimeout(function(){
+      var _img = container.find('img:first');
+      var w = _img.css('width');
+      var h = _img.css('height');
+      console.log(w,h);
+      $('.ic_caption',container).css({width:w});
+      container.css({width:w , height:h});
+    }, 1500);
+
+  });
+
+});
---
+++ js/main.js
@@ -0,0 +1,52 @@
+$('#nav-icon3,#nav-icon3grey').click(
+  function(){
+    $('.navmenu').toggleClass('hide')
+  }
+)
+
+
+$(document).ready(function(){
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4,#nav-icon3grey').click(function(){
+        $(this).toggleClass('open');
+    });
+});
+
+
+$(document).ready(function(){
+  if ($('.loadingpage').hasClass('.byebye')) {
+    $('#wrap').removeClass('hideme');
+  }
+
+  if ($('.loadingpage').not('.byebye')) {
+    $('#wrap').addClass('hideme');
+  }
+});
+
+$('.enter').click(function(){
+   $('.loadingpage').addClass('byebye');
+   $('#wrap').removeClass('hideme');
+});
+
+
+$('.bigimage').scroll(function(){
+    $("div").removeClass('hide');
+    $(".bigimage").addClass('hide');
+  });
+
+$('.casestudy').click(function(){
+    $('#projectpage div').removeClass('hide');
+    $(".bigimage").addClass('hide');
+  });
+
+
+  $(document).ready(function(){
+
+     var current_width = $(window).width();
+     //do something with the width value here!
+     if(current_width <= 875)
+     $('#projectpage div').removeClass('hide');
+     if(current_width <= 875)
+     $('#workpage #carousel').addClass('noshow');
+     if(current_width <= 875)
+     $('.mobileworkitems').removeClass('noshow');
+     });
